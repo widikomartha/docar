b9a2150aaa7a926025bbefa60fd2d68331606b83
Wiko <widikomartha@gmail.com>
2020-01-14 01:13:39 +0800

    commit

--- app/src/main/java/com/example/docar/LoginActivity.java
+++ app/src/main/java/com/example/docar/LoginActivity.java
@@ -1,4 +1,13 @@
 package com.example.docar;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
