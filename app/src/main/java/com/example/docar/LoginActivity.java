d11d9867d513a8b9ad218b80eacfcc08462608cf
Wiko <widikomartha@gmail.com>
2020-01-14 18:50:47 +0800

    toolbar

--- app/src/main/java/com/example/docar/LoginActivity.java
+++ app/src/main/java/com/example/docar/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.example.docar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button signin,signup;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signin = (Button) findViewById(R.id.signin);
+
+        Button btnLogin =  (Button)findViewById(R.id.signin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
