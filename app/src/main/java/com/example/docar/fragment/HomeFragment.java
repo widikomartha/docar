5b0f5b21b0aa97c5775c6aa2ab3ec9a6c4807030
Wiko <widikomartha@gmail.com>
2020-01-15 07:03:03 +0800

    tab

--- app/src/main/java/com/example/docar/fragment/HomeFragment.java
+++ app/src/main/java/com/example/docar/fragment/HomeFragment.java
@@ -1,10 +1,12 @@
-package com.example.docar;
+package com.example.docar.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.docar.R;
 
 public class HomeFragment extends Fragment {
 
